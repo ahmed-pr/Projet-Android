2b5e10a718f8ece7f6298d7ff85a69ac6b9b29a9
AduohPhalange <houdaelbiache@gmail.com>
2021-04-04 20:37:11 +0100

    Add Login and Registration activity

---
+++ app/src/main/java/com/android/vaccinationapp/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.android.vaccinationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView signup = findViewById(R.id.txtViewSignUp);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+    }
+}
