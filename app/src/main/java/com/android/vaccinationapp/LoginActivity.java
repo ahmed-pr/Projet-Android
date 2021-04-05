d01469e60ea263f3a3b2384e3848dbccfbf91773
AduohPhalange <houdaelbiache@gmail.com>
2021-04-05 20:27:07 +0100

    Add Home Page

--- app/src/main/java/com/android/vaccinationapp/LoginActivity.java
+++ app/src/main/java/com/android/vaccinationapp/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
@@ -15,6 +16,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btnlogin = findViewById(R.id.btnlogin);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
+            }
+        });
 
         TextView signup = findViewById(R.id.txtViewSignUp);
         signup.setOnClickListener(new View.OnClickListener() {
