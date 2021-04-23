30e98ed498ab416c95661d5b1af04a1143f495e6
AduohPhalange <houdaelbiache@gmail.com>
2021-04-23 07:22:28 +0000

    fixing UI

--- app/src/main/java/com/android/vaccinationapp/WelcomeActivity.java
+++ app/src/main/java/com/android/vaccinationapp/WelcomeActivity.java
@@ -7,37 +7,42 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.android.vaccinationapp.firestore.UsersFirestoreManager;
-
-public class MainActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity {
 
     private final static int SECOND_CALL_ID = 1234;
     private Button btnLogin;
+    private Button btnRegister;
 
-    private UsersFirestoreManager usersFirestoreManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_welcome);
 
-        usersFirestoreManager = UsersFirestoreManager.newInstance();
 
         btnLogin = findViewById(R.id.button);
-        /*btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                usersFirestoreManager.sendUsersBulk();
-                Toast.makeText(MainActivity.this, "Bulk users sent", Toast.LENGTH_LONG).show();
-            }
-        });*/
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(
-                        MainActivity.this,
+                        WelcomeActivity.this,
                         LoginActivity.class
+                );
+                startActivityForResult(intent, SECOND_CALL_ID);
+
+            }
+        });
+
+        btnRegister = findViewById(R.id.button2);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        WelcomeActivity.this,
+                        RegisterActivity.class
                 );
                 startActivityForResult(intent, SECOND_CALL_ID);
 
