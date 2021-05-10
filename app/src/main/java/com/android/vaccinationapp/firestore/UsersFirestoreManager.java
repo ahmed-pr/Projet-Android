3dd81bda40fdb00856329ecbe84b65c462e4b55d
HoudaELB <58733822+HoudaELB@users.noreply.github.com>
2021-05-10 22:12:57 +0000

    Update UsersFirestoreManager.java

--- app/src/main/java/com/android/vaccinationapp/firestore/UsersFirestoreManager.java
+++ app/src/main/java/com/android/vaccinationapp/firestore/UsersFirestoreManager.java
@@ -30,9 +30,6 @@ public class UsersFirestoreManager {
         usersCollectionReference.add(user);
     }
 
-    public void sendUsersBulk() {
-        createDocument(new User("EL BIACHE Houda", "houdaelbiache@gmail.com", "0653818691", "CD286763", 21, "Fès"));
-    }
-
+    
 }
 
